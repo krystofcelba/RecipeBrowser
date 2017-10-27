d057b62de4d105de94f72cd8a88e7e8dff4c6a2b
Krystof Celba <krystof.celba@icloud.com>
2017-10-27 17:03:13 +0200

    [#8] feat: dispatch fetchRecipes action on RecipesList mount

--- src/screens/RecipesList.tsx
+++ src/screens/RecipesList.tsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 
-class RecipesList extends Component {
+import { actionCreators } from '../redux/reducers/recipes';
+
+interface Props {
+  fetchRecipes: typeof actionCreators.fetchRecipes;
+}
+
+class RecipesList extends Component<Props, any> {
+  componentDidMount() {
+    this.props.fetchRecipes();
+  }
+
   render() {
     return <View style={styles.container} />;
   }
@@ -13,4 +24,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default RecipesList;
+const mapDispatchToProps = {
+  fetchRecipes: actionCreators.fetchRecipes,
+};
+
+export default connect(undefined, mapDispatchToProps)(RecipesList);
