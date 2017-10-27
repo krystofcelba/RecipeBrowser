da2f6c5228477e644259ec5b8e87bbc6219e731d
Krystof Celba <krystof.celba@icloud.com>
2017-10-27 07:35:56 +0200

    [#1] feat: change code to use react-native-navigation

---
+++ src/screens/RecipesList.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+class RecipesList extends Component {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default RecipesList;
