132a6b0cb3f013f8100eebcadc0cfd8291c95b16
Krystof Celba <krystof.celba@icloud.com>
2017-10-27 10:29:46 +0200

    [#4] feat: initially setup redux-saga

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,11 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers';
+import rootSaga from './sagas';
+  const sagaMiddleware = createSagaMiddleware();
 
-const configureStore = () => {
   const composeEnhancers = __DEV__
     ? composeWithDevTools({
         realtime: true,
@@ -12,8 +14,12 @@ const configureStore = () => {
       })
     : compose;
 
-  const store = createStore(rootReducer, composeEnhancers());
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(sagaMiddleware)),
+  );
 
+  sagaMiddleware.run(rootSaga);
   return store;
 };
 
