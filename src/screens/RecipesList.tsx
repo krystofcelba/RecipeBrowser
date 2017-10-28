09ea54e72e8eb26ff9056e1ca7b19d27530bb80d
Krystof Celba <krystof.celba@icloud.com>
2017-10-29 01:24:27 +0200

    [#8] feat: implement RecipesList screen

--- src/screens/RecipesList.tsx
+++ src/screens/RecipesList.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
 import { actionCreators } from '../redux/reducers/recipes';
+import { Recipe } from '../services/api';
+import RecipeCard from '../components/recipes-list/RecipeCard';
 
 interface Props {
   fetchRecipes: typeof actionCreators.fetchRecipes;
+  recipes: [Recipe];
 }
 
 class RecipesList extends Component<Props, any> {
@@ -13,19 +16,36 @@ class RecipesList extends Component<Props, any> {
     this.props.fetchRecipes();
   }
 
+  onPressRecipe = (recipeId: number) => {
+    console.log('on press recipe: ', recipeId);
+  };
+
   render() {
-    return <View style={styles.container} />;
+    const { recipes } = this.props;
+    console.log(this.props.recipes);
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={recipes}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <RecipeCard recipe={item as Recipe} onPress={this.onPressRecipe} />}
+        />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginBottom: 15,
   },
 });
+
+const mapStateToProps = ({ recipes: { recipesById, recipes } }) => ({ recipes: recipesById.map(id => recipes[id]) });
 
 const mapDispatchToProps = {
   fetchRecipes: actionCreators.fetchRecipes,
 };
 
-export default connect(undefined, mapDispatchToProps)(RecipesList);
+export default connect(mapStateToProps, mapDispatchToProps)(RecipesList);
