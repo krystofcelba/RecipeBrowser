f76b9a76dd77fb87fbec016326fd27e89cbb93ad
Krystof Celba <krystof.celba@icloud.com>
2017-10-29 06:07:39 +0100

    [#11] feat: add selectors
    - add getRecipesSearchInputText selector
    - add getRecipesById, getRecipes selectors
    - add getFilteredRecipes selector for getting array of recipes filtered by ingredient typed in search input

---
+++ src/redux/selectors/index.ts
@@ -0,0 +1,24 @@
+import { createSelector } from 'reselect';
+
+import { RootState } from '../reducers';
+import { Recipe } from 'src/services/api';
+
+export const getRecipesSearchInputText = (state: RootState) => state.ui.recipesList.searchInput.text.toLowerCase();
+export const getRecipesById = (state: RootState) => state.recipes.recipesById;
+export const getRecipes = (state: RootState) => state.recipes.recipes;
+
+export const getFilteredRecipes = createSelector(
+  [getRecipesSearchInputText, getRecipesById, getRecipes],
+  (searchInputText, recipesById, recipes): Recipe[] => {
+    return recipesById.reduce(
+      (filtered, id) => {
+        const recipe = recipes[id];
+        if (searchInputText.length === 0 || recipe.ingredientsNames.includes(searchInputText)) {
+          return [...filtered, recipe];
+        }
+        return filtered;
+      },
+      [] as Recipe[],
+    );
+  },
+);
