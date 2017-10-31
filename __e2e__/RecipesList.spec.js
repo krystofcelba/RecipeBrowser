392691320cbed3883e455b7ceabcff57fee6704e
Krystof Celba <krystof.celba@icloud.com>
2017-10-31 18:19:32 +0100

    [#20] test: describe recipe searching detox scenario

---
+++ __e2e__/RecipesList.spec.js
@@ -0,0 +1,17 @@
+describe('RecipesList screen', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should be able to search for a recipe with grapefruits and see its detail', async () => {
+    await element(by.id('topSearchBar')).tap();
+    await element(by.id('topSearchBar')).typeText('grape\n');
+    await expect(element(by.id('recipeCard_2'))).toNotExist();
+
+    // await expect(element(by.id('recipeCard_31'))).toBeVisible();
+
+    await element(by.id('recipeCard_31')).tap();
+
+    await expect(element(by.id('recipeDetailScreen'))).toBeVisible();
+  });
+});
