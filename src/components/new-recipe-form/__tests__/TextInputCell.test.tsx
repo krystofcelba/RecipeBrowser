145c99df9de112b9f6ad2035f2c2daa125abea35
Krystof Celba <krystof.celba@icloud.com>
2017-11-02 16:43:33 +0100

    [#29] test: add snapshot test of TextInputCell with index

--- src/components/new-recipe-form/__tests__/TextInputCell.test.tsx
+++ src/components/new-recipe-form/__tests__/TextInputCell.test.tsx
@@ -5,7 +5,12 @@ import TextInputCell from '../TextInputCell';
 
 describe('FormInputCell component', () => {
   it('renders as expected', () => {
-    const wrapper = shallow(<TextInputCell name="input" placeholder="placeholder" value="" onChangeText={() => {}} />);
+    const wrapper = shallow(<TextInputCell name="input" placeholder="placeholder" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders as expected with index', () => {
+    const wrapper = shallow(<TextInputCell index={0} name="input" placeholder="placeholder" />);
     expect(wrapper).toMatchSnapshot();
   });
 });
